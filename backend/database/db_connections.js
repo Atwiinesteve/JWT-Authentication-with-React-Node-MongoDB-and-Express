34f98651034bad8fb6535300a5733af457a43d06
KIIZA STEPHEN <53976517+Atwiinesteve@users.noreply.github.com>
2023-03-21 14:47:46 +0000

    implemented router in react

--- backend/database/db_connections.js
+++ backend/database/db_connections.js
@@ -2,12 +2,10 @@
 const mongoose = require('mongoose');
 require('dotenv/config');
 
-const connect = mongoose.connect(process.env.DATABASE_URL, {}).then((response) => {
-    if(response.status === 2000) {
-        console.log(`Database Connection Established Successfully..`)
-    }
+const connect = mongoose.connect(process.env.DATABASE_URL, {}).then(() => {
+    console.log(`Database Connection Established Successfully..`)
 }).catch((error) => {
     console.log(`Database Connection Failed: ${error.message}`)
 });
 
-module.exports = connect;
+module.exports = {connect};
