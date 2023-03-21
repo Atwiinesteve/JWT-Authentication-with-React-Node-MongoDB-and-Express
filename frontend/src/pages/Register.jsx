103b1165386fe3d15bde18dc726223763cf7a438
KIIZA STEPHEN <53976517+Atwiinesteve@users.noreply.github.com>
2023-03-21 15:01:04 +0000

    added register UI

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,7 +1,28 @@
 import React from 'react'
+import {Link} from 'react-router-dom';
+import {ToastContainer, toast} from 'react-toastify';
 
 export default function Register() {
   return (
-    <div>Register</div>
+    <>
+      <div className="container">
+        <h2>Register Account</h2>
+        <form>
+          <div>
+            <label htmlFor="email">Email</label>
+            <input type="email" name='' />
+          </div>
+          <div>
+            <label htmlFor="password">Password</label>
+            <input type="password" name='' />
+          </div>
+          <button type='submit'>Register</button>
+          <span>
+            <p>Already have an account? <Link to={'/login'}>Login</Link></p>
+          </span>
+        </form>
+        <ToastContainer />
+      </div>
+    </>
   )
 };
