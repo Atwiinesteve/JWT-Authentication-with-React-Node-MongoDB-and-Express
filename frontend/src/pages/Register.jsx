a5d0b0afdbafa09c411b354391130b13a182fbf7
KIIZA STEPHEN <53976517+Atwiinesteve@users.noreply.github.com>
2023-03-21 15:08:12 +0000

    handled data input in register formAC

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,28 +1,50 @@
-import React from 'react'
-import {Link} from 'react-router-dom';
-import {ToastContainer, toast} from 'react-toastify';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { ToastContainer, toast } from "react-toastify";
 
+// register component.
 export default function Register() {
-  return (
-    <>
-      <div className="container">
-        <h2>Register Account</h2>
-        <form>
-          <div>
-            <label htmlFor="email">Email</label>
-            <input type="email" name='' />
-          </div>
-          <div>
-            <label htmlFor="password">Password</label>
-            <input type="password" name='' />
-          </div>
-          <button type='submit'>Register</button>
-          <span>
-            <p>Already have an account? <Link to={'/login'}>Login</Link></p>
-          </span>
-        </form>
-        <ToastContainer />
-      </div>
-    </>
-  )
-};
+
+	// input values
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+
+  // on submit data
+  function handleSubmit(e) {
+    e.preventDefault();
+  }
+
+	// return component elemts
+	return (
+		<>
+			<div className="container">
+				<h2>Register Account</h2>
+				<form onSubmit={(e) => handleSubmit(e)}>
+					<div>
+						<label htmlFor="email">Email</label>
+						<input
+							type="email"
+							name={email}
+							onChange={(e) => setEmail(e.target.value)}
+						/>
+					</div>
+					<div>
+						<label htmlFor="password">Password</label>
+						<input
+							type="password"
+							name={password}
+							onChange={(e) => setPassword(e.target.value)}
+						/>
+					</div>
+					<button type="submit">Register</button>
+					<span>
+						<p>
+							Already have an account? <Link to={"/login"}>Login</Link>
+						</p>
+					</span>
+				</form>
+				<ToastContainer />
+			</div>
+		</>
+	);
+}
